6b5f22c18a7f477911204dd08928aa8a0b62e2de
ruofeng <weiruofeng@ruofengx.cn>
2022-02-20 19:18:41 +0800

    update caution

--- attach.sh
+++ attach.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-caution="Caution: Using Ctrl+P then Ctrl+Q to quit!"
+caution="警告Caution: 使用Using Ctrl+P 之后then Ctrl+Q 来to 退出quit!"
 echo "#================================================="
 echo -e "\033[41;37m ${caution} \033[0m"
 echo "Attaching to meta_core container..."
