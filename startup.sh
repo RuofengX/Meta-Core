bc4107c0c5bd60222165f94e77651943afb3ac77
ruofeng <weiruofeng@ruofengx.cn>
2022-02-20 19:18:54 +0800

    加入更新子模块语句

--- startup.sh
+++ startup.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+git submodule update --init --recursive  # 更新子模块
 sudo docker-compose up --build -d --remove-orphans
--- startup_cold.sh
+++ startup_cold.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo docker rmi registry.cn-shanghai.aliyuncs.com/1ris/game-core:latest
+git submodule update --init --recursive  # 更新子模块
+sudo docker rmi registry.cn-shanghai.aliyuncs.com/1ris/game-core:latest  # 删除旧版本镜像
 sudo docker-compose up --build -d --remove-orphans
