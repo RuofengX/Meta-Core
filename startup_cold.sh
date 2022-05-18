4304853a629275678e082534e2c7858d0229850c
weiruofeng <weiruofeng@ruofengx.cn>
2022-05-18 11:17:43 +0800

    Less dependence

--- startup_cold.sh
+++ startup_cold.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#使用这个脚本将docker冷启动服务器
 git submodule update --init --recursive  # 更新子模块
 sudo docker rmi registry.cn-shanghai.aliyuncs.com/1ris/game-core:latest  # 删除旧版本镜像
 sudo docker-compose up --build -d --remove-orphans
