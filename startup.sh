2a56a3ef022321167b5e125d151e2e8c675a8109
weiruofeng <weiruofeng@ruofengx.cn>
2022-05-18 11:29:05 +0800

    add push script

--- startup.sh
+++ startup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 #使用这个脚本将从docker启动服务器
+cd minecraft-banlist
+./minecraft-banlist/push.sh
+cd ..
 git submodule update --init --recursive  # 更新子模块
 sudo docker-compose up --build -d --remove-orphans
