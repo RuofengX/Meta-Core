adeaa829952abd9287e3b0dc7566ee866f8e966f
weiruofeng <weiruofeng@ruofengx.cn>
2022-05-31 22:39:05 +0800

    添加了说明

--- startup.sh
+++ startup.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 #使用这个脚本将从docker启动服务器
-cd minecraft-banlist
-./minecraft-banlist/push.sh
-cd ..
 git submodule update --init --recursive  # 更新子模块
 sudo docker-compose up --build -d --remove-orphans
